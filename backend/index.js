55d1199b2c1fa02c81567c7b912fad948452280e
Racknar0 <racknarow1@gmail.com>
2022-03-10 21:26:01 -0500

    add modules

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
-import { express } from "express";
+import express from "express";
 
+const app = express();
 
+app.use('/', (req , res) => {
+    res.send('Test')
+})
+
+app.listen(4000, () => {
+    console.log('Servidor Funcionando en el Puerto 4000');
+});
