36091be7b4fc4a9b575c128470600a672e535d08
Racknar0 <racknarow1@gmail.com>
2022-03-10 23:40:54 -0500

    Add DB Registers

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,7 +1,22 @@
+import Veterinario from "../models/Veterinario.js"
+
+const registrar = async (req, res) => {
+    /* const {email, password, nombre } = req.body */
+
+    try {
+        //Guardar un nuevo Veterinario
+        const veterinario = new Veterinario(req.body);
+        const veterinarioGuardado = await veterinario.save()
+
+        res.json({ veterinarioGuardado })
+    } catch (error) {
+        console.log(error);
+    }
 
 
-const registrar = (req, res) => {
-    res.json({ msg: 'Registrando Usuario...' })
+   
+
+    
 };
 
 const perfil = (req, res) => {
