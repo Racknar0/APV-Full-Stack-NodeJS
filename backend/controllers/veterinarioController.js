c3192d12dd5c19ab2b4da53982f7d162120c15b6
Racknar0 <racknarow1@gmail.com>
2022-03-11 21:17:00 -0500

    Add User Auth

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -53,10 +53,19 @@ const confirmar = async (req, res) => {
     }
 }
 
-const autenticar = (req, res) => {
+const autenticar = async (req, res) => {
+    const { email } = req.body;
 
-    console.log(req.body);
-    res.json({msg: 'Autenticando'})
+    // Comprobar que el usuario existe
+    const usuario = await Veterinario.findOne({email})
+
+    if ( !usuario ) {
+        const error = new Error('El usuario no existe')
+        return res.status(404).json({msg: error.message})
+    }
+    
+    // Comprobar si el usuario esta confirmado o no
+    
 }
 
 export { registrar, perfil, confirmar, autenticar }
