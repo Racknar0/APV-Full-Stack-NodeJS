6bd82c5caf314193d982587d1f5ec13e3aa5ae67
Racknar0 <racknarow1@gmail.com>
2022-03-12 00:01:53 -0500

    Genrerar ALerta Token

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -100,8 +100,17 @@ const olvidePassword = async (req, res) => {
         console.log(error);
     }
 }
-const comprobarToken = (req, res) => {
+const comprobarToken = async (req, res) => {
+    const {token} = req.params
 
+    const tokenValido = await Veterinario.findOne({ token })
+    if(tokenValido) {
+        //El token es valido el usuario existe
+        res.json({msg: 'Token Valido y el usuario existe'})
+    } else {
+        const error = new Error('Token no valido')
+        return res.status(400).json({msg: error.message})
+    }
 }
 const nuevoPassword = (req, res) => {
 
