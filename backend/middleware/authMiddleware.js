3f544053a0dfd0b46bd4156af6942492252b8c39
Racknar0 <racknarow1@gmail.com>
2022-03-11 23:26:39 -0500

    Auth Token

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,22 +7,20 @@ const checkAuth = async ( req , res, next) => {
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))  {
         console.log('SI tiene el token con bearer');
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    
+            req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado")
+    
+            return next();
+    
+        } catch (error) {
+            const  e = new Error('Token No valido')
+            return res.status(403).json({msg: e.message});
+        }
     } 
-
-    try {
-        token = req.headers.authorization.split(' ')[1]
-        
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
-        req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado")
-
-        return next();
-
-    } catch (error) {
-        const  e = new Error('Token No valido')
-        return res.status(403).json({msg: e.message});
-    }
-
 
     if (!token) {
         const  error = new Error('Token No valido o Inexistente')
