54461bb56bea00032132952ee5a5ebc8ddfa3375
Racknar0 <racknarow1@gmail.com>
2022-03-12 14:38:10 -0500

    Obtain Specific Paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -18,6 +18,49 @@ const obtenerPacientes = async (req, res) => {
     res.json(pacientes)
 }
 
+const obtenerPaciente = async (req, res) => {
+    const {id} = req.params
+    const paciente = await Paciente.findById(id)
 
+    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: 'Acción no valida'})
+    }
 
-export { agregarPaciente , obtenerPacientes }
+    if(paciente) {
+        res.json(paciente)
+    }
+}
+
+const actualizarPaciente = async (req, res) => {
+
+    if(!paciente) {
+        return res.status(404).json({msg: 'No encontrado'})
+    }
+
+    const {id} = req.params
+    const paciente = await Paciente.findById(id)
+    
+    if (paciente.veterinario._id !== req.veterinario._id.toString()) {
+        return res.json({msg: 'Acción no valida'})
+    }
+
+    // Actualizar Paciente
+    paciente.nombre = req.body.nombre
+    
+    try {
+        const pacienteActualizado = await paciente.save();
+        res.json(pacienteActualizado)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const eliminarPaciente = async (req, res) => {}
+
+export { 
+    agregarPaciente , 
+    obtenerPacientes, 
+    obtenerPaciente,  
+    actualizarPaciente , 
+    eliminarPaciente 
+}
--- backend/routes/pacienteRoutes.js
+++ backend/routes/pacienteRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { agregarPaciente , obtenerPacientes } from '../controllers/pacienteController.js'
+import { 
+    agregarPaciente , 
+    obtenerPacientes, 
+    obtenerPaciente,  
+    actualizarPaciente , 
+    eliminarPaciente 
+} from '../controllers/pacienteController.js'
 import checkAuth from "../middleware/authMiddleware.js"
 
 
@@ -8,5 +14,11 @@ router
     .route('/')
     .post(checkAuth, agregarPaciente)
     .get(checkAuth, obtenerPacientes);
+
+router 
+    .route('/:id')
+    .get(checkAuth, obtenerPaciente)
+    .put(checkAuth, actualizarPaciente)
+    .delete(checkAuth, eliminarPaciente)
 
 export default router;
