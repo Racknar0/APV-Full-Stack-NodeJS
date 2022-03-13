47e48353600bc9af58d0ecd4ce810ac73b996db8
Racknar0 <racknarow1@gmail.com>
2022-03-13 17:52:04 -0500

    Send Olvide Pass

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,6 +1,40 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
+import Alerta from '../components/Alerta'
+import clienteAxios from '../config/axios'
 
 const OlvidePassword = () => {
+    const [email, setEmail] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        if ( email === '' || email.length < 6) {
+            setAlerta({msg: 'El email es Obligatorio', error: true})
+            return
+        }
+
+        try {
+            
+            const { data } = await clienteAxios.post('/veterinarios/olvide-password', { email })
+
+            console.log( data );
+
+            setAlerta({msg: data.msg})
+
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+
+
+    }
+
+    const {msg} = alerta
+
     return (
         <>
             
@@ -12,11 +46,16 @@ const OlvidePassword = () => {
             </div>
 
             <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-                <form action="">
+
+            { msg && <Alerta 
+                    alerta={alerta} /> 
+            }
+
+                <form onSubmit={handleSubmit}>
 
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
-                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={email} onChange={ e => setEmail(e.target.value)} />
                     </div>
 
                     <input type="submit" value="Enviar Instrucciones" className='bg-indigo-700 w-full py-3 px-10 rounded-lg text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto '/>
