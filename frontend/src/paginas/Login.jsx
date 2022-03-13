a1130f02d2ad66ad6bff61fe66552fc1ef32f365
Racknar0 <racknarow1@gmail.com>
2022-03-12 21:54:25 -0500

    Register Page Tailwind

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -5,10 +5,10 @@ const login = () => {
         <>
             <div>
                 <h1 className="text-indigo-600 font-black text-6xl">
-                    Inicia Sesión y Administra tus <span className="text-black">Pacientes</span>
+                    Inicia Sesión y Administra tus{' '}<span className="text-black">Pacientes</span>
                 </h1>
             </div>
-            <div>
+            <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
                 <form action="">
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
