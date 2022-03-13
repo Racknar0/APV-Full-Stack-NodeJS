20269568bbba4fed11b433164d3343fc50168e24
Racknar0 <racknarow1@gmail.com>
2022-03-12 22:15:53 -0500

    Form Validation

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Registrar = () => {
+
+    const [ nombre, setNombre ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
+    const [ repetirPassword, setRepetirPassword ] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if([nombre, email, password ,repetirPassword].includes('')) {
+            console.log('Hay campos vacíos');
+            return;
+        }
+
+        if( password !== repetirPassword ) {
+            console.log('Los password no son iguales');
+            return;
+        }
+
+        if( password.length < 6) {
+            console.log('el password es muy corto, agrega mínimo 6 caracteres');
+        }
+        
+        
+    }
+
     return (
         <>
             <div>
@@ -11,23 +38,25 @@ const Registrar = () => {
             </div>
 
             <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-                <form action="">
+                <form 
+                onSubmit={handleSubmit}
+                >
 
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Nombre</label>
-                        <input type="text" placeholder='Tu Nombre' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="text" placeholder='Tu Nombre' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={nombre} onChange={ e => setNombre (e.target.value)} />
                     </div>
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
-                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={email} onChange={ e => setEmail (e.target.value)} />
                     </div>
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Password</label>
-                        <input type="password" placeholder='Tu password' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="password" placeholder='Tu password' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={password} onChange={ e => setPassword (e.target.value)} />
                     </div>
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Repetir Password</label>
-                        <input type="password" placeholder='Repite tu password' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="password" placeholder='Repite tu password' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={repetirPassword} onChange={ e => setRepetirPassword (e.target.value)} />
                     </div>
 
 
