06c259038e1e4040e179a02288ba2a60a5424daf
Racknar0 <racknarow1@gmail.com>
2022-03-13 20:37:33 -0500

    Add Token Local Storage

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import useAuth from '../hooks/useAuth';
+import Alerta from '../components/Alerta';
+import clienteAxios from '../config/axios';
 
 const login = () => {
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [alerta, setAlerta] = useState({})
 
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        if ([email, password].includes('')) {
+            setAlerta({
+                msg: 'Todos los campos son Obligatorios', error: true
+            })
+            return;
+        }
+
+        try {
+            const {data} = await clienteAxios.post('/veterinarios/login', { email, password })
+            localStorage.setItem('token', data.token)
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+    }
+
+    const { msg } = alerta
 
     return (
         <>
@@ -13,14 +41,20 @@ const login = () => {
                 </h1>
             </div>
             <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-                <form action="">
+
+            {msg && 
+               <Alerta 
+                    alerta={alerta}
+            />}
+
+                <form onSubmit={handleSubmit}>
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Email</label>
-                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="email" placeholder='Email de registro' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={email} onChange={ e => setEmail (e.target.value)}/>
                     </div>
                     <div className='my-5'>
                         <label htmlFor="" className='uppercase text-gray-600 block text-xl font-bold'>Password</label>
-                        <input type="password" placeholder='Tu password' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl'  />
+                        <input type="password" placeholder='Tu password' className='border w-full p-3 mt-3 bg-gray-50 rounded-xl' value={password} onChange={ e => setPassword (e.target.value)} />
                     </div>
 
                     <input type="submit" value="Iniciar Sesión" className='bg-indigo-700 w-full py-3 px-10 rounded-lg text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto '/>
