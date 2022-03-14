92fdbdb4e52cb2e14fe20acf2871d2c436b33031
Racknar0 <racknarow1@gmail.com>
2022-03-13 21:06:29 -0500

    Create Routing Protected Routes

---
+++ frontend/src/layout/RutaProtegida.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+
+
+const RutaProtegida = () => {
+  return (
+
+    <>
+
+        <h1>Esta es una ruta protegida</h1>
+
+        <Outlet />
+
+    </>
+  )
+}
+
+export default RutaProtegida
---
+++ frontend/src/paginas/AdministrarPacientes.jsx
@@ -0,0 +1,8 @@
+
+const AdministrarPacientes = () => {
+  return (
+    <h1>Desde Admin Pacientes</h1>
+  )
+}
+
+export default AdministrarPacientes
