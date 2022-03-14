f48c0c1934a18842545959fa5e8550818bd83ae3
Racknar0 <racknarow1@gmail.com>
2022-03-13 21:40:07 -0500

    Add Header

--- frontend/src/layout/RutaProtegida.jsx
+++ frontend/src/layout/RutaProtegida.jsx
@@ -1,21 +1,21 @@
 import { Outlet, Navigate } from 'react-router-dom'
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 import useAuth from '../hooks/useAuth'
 
 const RutaProtegida = () => {
 
     const { auth, cargando } = useAuth()
 
-    console.log(auth);
-    console.log(cargando);
-
     if( cargando ) return 'Cargando...'
   return (
 
     <>
-
-        <h1>Esta es una ruta protegida</h1>
-
-        {auth?._id ? <Outlet /> : <Navigate to='/'/>}
+        <Header />
+            {auth?._id ? <Outlet /> : <Navigate to='/'/>}
+        <Footer />
     </>
   )
 }
